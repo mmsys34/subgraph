425e6b21d5c3bbfe71b816876c29f23d832262c6
mmsys34 <leon@more.markets>
2024-12-03 13:01:54 +0700

    feat: token price for testnet

--- src/fetchUsdTokenPrice.ts
+++ src/fetchUsdTokenPrice.ts
@@ -2,23 +2,67 @@ import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 import { ChainlinkPriceFeed } from "../generated/MorphoBlue/ChainlinkPriceFeed";
 
-const ankrFlow = Address.fromString(
-  "0x1b97100ea1d7126c4d60027e231ea4cb25314bdb"
-).toHexString();
-
 const wflow = Address.fromString(
-  "0xd3bF53DAC106A0290B0483EcBC89d40FcC961f3e"
+  "0xe0fd0a2a4c2e59a479aab0cf44244e355c508766"
 ).toHexString();
 
-const usdPriceFeeds = new Map<string, string>().set(
-  wflow,
-  Address.fromString("0x0a4224D61A53932C920F673fE6c4B70Fb0D54345").toHexString()
-);
+const usdcf = Address.fromString(
+  "0x5e65b6b04fba51d95409712978cb91e99d93ae73"
+).toHexString();
 
-const relatePriceFeeds = new Map<string, string>().set(
-  ankrFlow,
-  Address.fromString("0xA363e8627b5b4A5DC1cf6b5f228665C5CafF770f").toHexString()
-);
+const usdf = Address.fromString(
+  "0xd7d43ab7b365f0d0789ae83f4385fa710ffdc98f"
+).toHexString();
+
+const btcf = Address.fromString(
+  "0x208d09d2a6dd176e3e95b3f0de172a7471c5b2d6"
+).toHexString();
+
+const ethf = Address.fromString(
+  "0x059a77239dafa770977dd9f1e98632c3e4559848"
+).toHexString();
+
+const ankrflow = Address.fromString(
+  "0xe132751ab5a14ac0bd3cb40571a9248ee7a2a9ea"
+).toHexString();
+
+const usdPriceFeeds = new Map<string, string>()
+  .set(
+    wflow,
+    Address.fromString(
+      "0x0a4224D61A53932C920F673fE6c4B70Fb0D54345"
+    ).toHexString()
+  )
+  .set(
+    usdcf,
+    Address.fromString(
+      "0xBEfB2b2B48fdEece45253b2eD008540a23d25AFE"
+    ).toHexString()
+  )
+  .set(
+    usdf,
+    Address.fromString(
+      "0x2e9EcBf2D63094A08c9ff5eb20A4EbBFfBFc12eD"
+    ).toHexString()
+  )
+  .set(
+    btcf,
+    Address.fromString(
+      "0xe65b5154aE462fD08faD32B2A85841803135894b"
+    ).toHexString()
+  )
+  .set(
+    ethf,
+    Address.fromString(
+      "0x2b40Fc7326E3bF1DB3571e414d006Ee42d49C427"
+    ).toHexString()
+  )
+  .set(
+    ankrflow,
+    Address.fromString(
+      "0x017efB6272Dc61DCcfc9a757c29Fd99187c9d208"
+    ).toHexString()
+  );
 
 function fetchPriceFromFeed(feedAddress: Address): BigDecimal {
   const chainlinkPriceFeed = ChainlinkPriceFeed.bind(feedAddress);
@@ -45,18 +89,6 @@ export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
     );
 
     return fetchPriceFromFeed(chainlinkPriceFeed);
-  } else if (relatePriceFeeds.has(tokenAddress.toHexString())) {
-    const flowPriceFeed = Address.fromString(usdPriceFeeds.get(wflow));
-    const flowPrice = fetchPriceFromFeed(flowPriceFeed);
-
-    const pairPriceFeed = Address.fromString(
-      relatePriceFeeds.get(tokenAddress.toHexString())
-    );
-    const pairPrice = fetchPairPriceFromFeed(pairPriceFeed);
-
-    return flowPrice
-      .times(pairPrice)
-      .div(BigInt.fromString("10").pow(36).toBigDecimal());
   }
 
   return BigDecimal.zero();
